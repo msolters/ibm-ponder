46e4753e42705d593478c556f5755022656bf5a7
Mark Solters <msolters@gmail.com>
2018-03-01 23:17:04 -0500

    Implement some new mutation operators

--- ponder-genetic.js
+++ ponder-genetic.js
@@ -1,8 +1,10 @@
 const N=6
 const maxScore = N*Math.pow(2, N)
-const P=100    // solution population size
-let D=5     // number of solutions replaced each cycle
-let mutationChance = 0.07
+const P=50      // solution population size
+const D=10       // number of solutions replaced each cycle
+const mutationChanceDefault = 0.001
+let mutationChance = mutationChanceDefault
+let replacing = 0
 
 const syms = ['A', 'E', 'I', 'O', 'U']
 // const syms = ['A', 'B', 'C']
@@ -18,6 +20,7 @@ function getNeighborMatrix() {
   }
   return M
 }
+
 function getBlankSyms() {
   let blankSyms = {}
   for ( let s of syms ) {
@@ -38,6 +41,10 @@ function createRandomAnswer() {
     randomAnswer.push(getRandomSymbol())
   }
   return randomAnswer
+}
+
+function createFlatAnswer() {
+  return Array(Math.pow(2, N)).fill(getRandomSymbol())
 }
 
 /**
@@ -70,8 +77,9 @@ let staticGenerations = 0
 function isMaxScoreDifferent(max) {
   if (max != lastMax) {
     staticGenerations = 0
-    mutationChance = 0.07
-    console.log(`${100*max}%`)
+    replacing = 0
+    mutationChance = mutationChanceDefault
+    // console.log(`${100*max}%`)
   } else {
     staticGenerations++
     // mutationChance += 0.00001
@@ -100,7 +108,19 @@ function computeScoresAndLikelihood(solutions) {
     offset += scores[s]/scoreTotal
     likelihood[s] = offset
   }
-  return [scores, likelihood]
+
+  //  Compute the inverse likelihood, used for finding the weakest solutions
+  let inverseLikelihood = []
+  let inverseTotal = scores.reduce( (s, i) => {
+    return s + Math.pow(i, -1)
+  }, 0)
+  inverseTotal = Math.pow(inverseTotal, -1)
+  offset = 0
+  for (let s in scores) {
+    offset += inverseTotal/scores[s]
+    inverseLikelihood.push(offset)
+  }
+  return [scores, likelihood, inverseLikelihood]
 }
 
 function getSolution() {
@@ -108,66 +128,120 @@ function getSolution() {
   let solutions = createRandomAnswers()
   // let solutions = createFlatAnswers()
   while(1) {
-    var scores, likelihood
+    var scores, likelihood, inverseLikelihood
 
     // Create P new solutions
     let newSolutions = []
     for (let p=0; p<D; p++) {
-      [scores, likelihood] = computeScoresAndLikelihood(solutions)
+      [scores, likelihood, inverseLikelihood] = computeScoresAndLikelihood(solutions)
 
       // Pick 2 random solutions
-      let s1 = solutions[getRandomSolution(likelihood)]
-      let s2 = solutions[getRandomSolution(likelihood)]
+      let s1_idx = getRandomSolution(likelihood)
+      let s2_idx = getRandomSolution(likelihood)
+      let s1 = solutions[s1_idx]
+      let s2 = solutions[s2_idx]
+      let new_s1 = []
+      let new_s2 = []
 
       // Perform cross over
       let slicePoint = Math.floor(Math.random()*s1.length)
-      let newSolution = []
       for (let k=0; k<s1.length; k++) {
         if (k < slicePoint) {
-          newSolution.push(s1[k])
+          new_s1.push(s1[k])
+          new_s2.push(s2[k])
         } else {
-          newSolution.push(s2[k])
+          new_s1.push(s2[k])
+          new_s2.push(s1[k])
         }
 
         // Apply random mutation
-        if (Math.random() < mutationChance) {
-          newSolution[k] = getRandomSymbol()
-        }
+        // if (Math.random() < mutationChance) {
+        //   new_s1[k] = getRandomSymbol()
+        // }
+        // if (Math.random() < mutationChance) {
+        //   new_s2[k] = getRandomSymbol()
+        // }
       }
 
-      //  Replace weakest solution
-      let idx = getWeakestSolutionIndex(scores)
-      solutions[idx] = newSolution
+      //  Replace weakest solutions
+      let [idx, idx2] = getWeakestSolutionIndex(scores)
+      solutions[idx] = new_s1
+      solutions[idx2] = new_s2
+      // solutions[idx] = (Math.random() < 0.5) ? new_s1 : new_s2
+      // solutions[getRandomSolution(inverseLikelihood)] = new_s1
+      // solutions[getRandomSolution(inverseLikelihood)] = new_s2
       // newSolutions.push(newSolution)
+
+      // Mutate
+      for (let s of solutions) {
+        for (let k in s) {
+          if (Math.random() < mutationChance) {
+            s[k] = getRandomSymbol()
+          }
+        }
+      }
     }
     let _maxScore = Math.max(...scores)/maxScore
     isMaxScoreDifferent(_maxScore)
-    if (staticGenerations > 0 && staticGenerations % 5000 == 0) {
+    if (staticGenerations > 0 && staticGenerations % 10 == 0) {
       // replace the weakest solution with a random one
-      console.log("replacing")
-      let idx = getWeakestSolutionIndex(scores)
-      solutions[idx] = getRandomSolution()
+      // let replacing = Math.floor(Math.random()*P)
+      replacing++
+      for (let k=0; k<Math.min(replacing, P); k++) {
+        let m = Math.random()
+        let idx = Math.floor(Math.random()*P)
+        if (m < 0.25) {
+          let p = Math.floor(Math.random()*solutions[idx].length)
+          let left = solutions[idx].slice(0, p)
+          let right = solutions[idx].slice(p, solutions[idx].length)
+          let newSolution = right.concat(left)
+          solutions[idx] = newSolution
+        } else if (m < 0.5) {
+          solutions[idx].reverse()
+        } else if (m < 0.75){
+          solutions[idx] = createRandomAnswer()
+        } else {
+          solutions[idx] = createFlatAnswer()
+        }
+      }
+      // printSolutions(solutions)
     }
     // solutions = newSolutions
+    console.log(`(${replacing}/${P}) ${100*_maxScore}%`)
+    // printSolutions(solutions)
   }
+}
+
+function printSolutions(solutions) {
+  for (let s of solutions) {
+    let str = ''
+    for (let sym of s) {
+      str += sym
+    }
+    console.log(str)
+  }
+  console.log('\n')
 }
 
 function getWeakestSolutionIndex(scores) {
   let min = maxScore
   let idx = 0
+  let idx2 = 0
   for (let s in scores) {
     if (scores[s] < min) {
       min = scores[s]
+      idx2 = idx
       idx = s
     }
   }
-  return idx
+  return [idx, idx2]
 }
 
-function getRandomSolution(likelihood) {
+function getRandomSolution(likelihood, p=1) {
   let L = {}
-  for (let t=0; t<10; t++) {
+  for (let t=0; t<p; t++) {
     let r = Math.random()
+    // console.log(r, likelihood)
     for (l in likelihood) {
       if (r < likelihood[l]) {
         if (L[l]) {
@@ -175,18 +249,22 @@ function getRandomSolution(likelihood) {
         } else {
           L[l] = 1
         }
+        break
       }
     }
   }
+  // console.log(L)
   let max = 0
-  var idx
+  var idx = []
   for (let l of Object.keys(L)) {
     if (L[l] > max) {
       max = L[l]
-      idx = l
+      idx = [l]
+    } else if (L[l] == max) {
+      idx.push(l)
     }
   }
-  return idx
+  return idx[Math.floor(Math.random()*idx.length)]
 }
 
 /**
